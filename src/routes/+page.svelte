e17cff3b2005be53fe946d301c9eae0ea959dd07
babakfp <44144724+babakfp@users.noreply.github.com>
2022-10-11 17:06:02 -0700

    removed tabs and used radiobtns instead

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,25 @@
 <script>
 	import {
-		Tab,
-		TabGroup,
-		TabList,
-		TabPanel,
-		TabPanels,
-	} from '@rgossiaux/svelte-headlessui'
+    RadioGroup,
+    RadioGroupLabel,
+    RadioGroupOption,
+    RadioGroupDescription,
+  } from '@rgossiaux/svelte-headlessui'
+
+  let keyType = 'public-key'
+
+	const keyTypeOptions = [
+		{
+			label: 'PUBLIC KEY',
+			value: 'public-key',
+			description: 'Entering a public key means you will need to enter a private key each time you post content (either manually or by Nostr browser extension).',
+		},
+		{
+			label: 'PRIVATE KEY',
+			value: 'private-key',
+			description: 'Entering a private key means Nostroid will automatically sign with your private key each time you post content.',
+		},
+	]
 </script>
 
 <h1 class="font-semibold text-xl">Welcome to Nostroid</h1>
@@ -16,7 +30,7 @@
 	In order to participate in The Nostr Network, you will need to have PUBLIC and PRIVATE key pairs.
 </p>
 
-<div class="mt-4 grid gap-4 p-6 text-sm border border-dashed border-gray-700 rounded">
+<div class="mt-4 grid gap-6 p-6 text-sm border border-dashed border-gray-700 rounded">
 	<div class="space-y-1">
 		<h2 class="font-semibold">PUBLIC KEY</h2>
 		<p class="text-gray-400 text-xs sm:text-sm">A publicly known unique ID, associated with your user on the Nostr network. Can be shared freely. Others can see your posts or follow you using only your public key.</p>
@@ -28,70 +42,43 @@
 	</div>
 </div>
 
-<p class="mt-4">
-	If you don't have a Nostr key pair you can either 
-	<b>generate</b> a new key pair or just take a <strong>look around</strong>.
+<p class="mt-6">
+	If you don't have a Nostr key pair, you can either 
+	<a class="link" href="/">take a look around</a> or generate a new key pair below.
 </p>
 
-<div class="mt-4 flex gap-4">
-	<button class="btn btn-brand btn-outline">Generate</button>
-	<a class="btn btn-gray btn-outline" href="/">Look around</a>
-</div>
+<form class="mt-6" on:submit|preventDefault>
 
-<div class="mt-4">
-	<h2>Enter your key pairs:</h2>
-	
-	<TabGroup class="mt-4">
-		<TabList class="flex gap-1 bg-gray-50/5 rounded p-1">
-			<Tab class="key-tab btn-base w-full text-xs">PUBLIC KEY</Tab>
-			<Tab class="key-tab btn-base w-full text-xs">PRIVATE KEY</Tab>
-		</TabList>
-		<TabPanels class="mt-2">
-			<TabPanel>
-				<form class="relative" on:submit|preventDefault>
-					<label class="flex items-center pr-2 border-2 border-gray-700 rounded" for>
-						<input class="w-full h-14 px-4 bg-transparent text-sm font-code placeholder:text-gray-600 placeholder:font-base" type="text" placeholder="Enter your PUBLIC KEY" />
-						<button class="btn btn-brand text-xs">PROCESS
-							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-								<path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
-							</svg>
-						</button>
-					</label>
-				</form>
-				<p class="mt-2 text-gray-400 text-xs">Entering a public key means you will need to enter a private key each time you post content (either manually or by Nostr browser extension).</p>
-			</TabPanel>
-			<TabPanel>
-				<form class="relative" on:submit|preventDefault>
-					<label class="flex items-center pr-2 border-2 border-gray-700 rounded" for>
-						<input class="w-full h-14 px-4 bg-transparent text-sm font-code placeholder:text-gray-600 placeholder:font-base" type="text" placeholder="Enter your PRIVATE KEY" />
-						<button class="btn btn-brand text-xs">PROCESS
-							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-								<path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
-							</svg>
-						</button>
-					</label>
-				</form>
-				<p class="mt-2 text-gray-400 text-xs">Entering a private key means Nostroid will automatically sign with your private key each time you post content.</p>
-			</TabPanel>
-		</TabPanels>
-	</TabGroup>
-</div>
+	<RadioGroup class="grid gap-2 text-sm sm:grid-cols-2 sm:gap-4" value={keyType} on:change={(e) => keyType = e.detail}>
+		{#each keyTypeOptions as option}
+			<RadioGroupOption value={option.value} let:checked>
+				<div class="h-full p-4 border-2 border-gray-800 rounded flex justify-between gap-4 duration-150 cursor-pointer {checked && 'bg-gray-800 border-transparent shadow-[0_0_4px_theme(colors.gray.600)]'}">
+					<div class="space-y-2">
+						<RadioGroupLabel>{option.label}</RadioGroupLabel>
+						<RadioGroupDescription class="text-gray-400 text-xs">{option.description}</RadioGroupDescription>
+					</div>
+					<div class="flex items-center justify-center w-6 min-w-6 h-6 border-2 border-gray-800 rounded-full duration-150 {checked && 'bg-gray-900 border-gray-900'}">
+						<svg class="w-3 h-3 scale-0 duration-150 {checked && 'scale-100'}" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+							<path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+						</svg>
+					</div>
+				</div>
+			</RadioGroupOption>
+		{/each}
+	</RadioGroup>
 
-<style lang="postcss">
-	:global(.key-tab) {
-		@apply relative;
-	}
-	:global(.key-tab::after) {
-		content: "";
-		@apply hide absolute top-full inset-x-center translate-y-0.5 duration-150 ease-in-out;
-		border-style: solid;
-		border-width: 4px 4px 0 4px;
-		border-color: theme(colors.gray.700) transparent transparent transparent;
-	}
-	:global(.key-tab[aria-selected="true"]) {
-		@apply bg-gray-700;
-	}
-	:global(.key-tab[aria-selected="true"]::after) {
-		@apply show;
-	}
-</style>
+	<button class="mt-6 btn btn-brand btn-outline">Generate
+		<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+			<path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+		</svg>
+	</button>
+
+	<label class="mt-2 flex items-center pr-2 border-2 border-gray-800 rounded" for>
+		<input class="w-full h-14 px-4 bg-transparent text-sm font-code placeholder:text-gray-600 placeholder:font-base" type="text" placeholder="Enter your {keyType.toUpperCase().replace('-', ' ')}" />
+		<button class="btn btn-brand text-xs">PROCESS
+			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+				<path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
+			</svg>
+		</button>
+	</label>
+</form>
