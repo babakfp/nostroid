18af60f1688af89a6550698bd51a36c11bb5bf3f
babakfp <44144724+babakfp@users.noreply.github.com>
2022-10-15 06:58:48 -0700

    added mobile bottom nav

---
+++ src/lib/parts/MobileBottomNav/Index.svelte
@@ -0,0 +1,13 @@
+<script>
+	import MobileBottomNavItem from './MobileBottomNavItem.svelte'
+</script>
+
+<!-- :| -->
+<div class="h-13" />
+
+<nav class="fixed bottom-0 inset-x-0 | flex justify-between | bg-gray-800">
+	<MobileBottomNavItem></MobileBottomNavItem>
+	<MobileBottomNavItem></MobileBottomNavItem>
+	<MobileBottomNavItem></MobileBottomNavItem>
+	<MobileBottomNavItem></MobileBottomNavItem>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script>
   import '$styles/app.postcss'
+	import MobileBottomNav from '$parts/MobileBottomNav/Index.svelte'
+
 </script>
 
-<main class="container py-8 pb-20">
+<main class="container py-8 pb-20 min-h-screen">
 	<slot />
 </main>
+
+<MobileBottomNav />
