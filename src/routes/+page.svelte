4cd7551ae683e37d4307dc577257a8329997c7c4
babakfp <44144724+babakfp@users.noreply.github.com>
2022-10-15 06:19:18 -0700

    fixed label overflowing on input and button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -66,9 +66,9 @@
 		</button>
 
 		<div class="mt-2 relative flex items-center pr-2 border-2 border-gray-800 rounded" for>
-			<label class="absolute inset-0" for="key-code"></label>
-			<input class="w-full h-14 px-4 bg-transparent text-sm font-code placeholder:text-gray-600 placeholder:font-base" type="text" id="key-code" placeholder="Enter your {keyType.toUpperCase().replace('-', ' ')}" />
-			<button class="btn btn-brand text-xs">PROCESS
+			<label class="absolute inset-0 cursor-text" for="key-code"></label>
+			<input class="relative w-full h-14 px-4 bg-transparent text-sm font-code placeholder:text-gray-600 placeholder:font-base" type="text" id="key-code" placeholder="Enter your {keyType.toUpperCase().replace('-', ' ')}" />
+			<button class="relative btn btn-brand text-xs">PROCESS
 				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
 					<path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
 				</svg>
