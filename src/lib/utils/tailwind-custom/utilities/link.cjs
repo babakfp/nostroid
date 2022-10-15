6f6928ce73f93716d0bd2abddb1717e997c033e0
babakfp <44144724+babakfp@users.noreply.github.com>
2022-10-15 06:37:18 -0700

    changed link util hover style

--- src/lib/utils/tailwind-custom/utilities/link.cjs
+++ src/lib/utils/tailwind-custom/utilities/link.cjs
@@ -4,6 +4,7 @@ module.exports = plugin(({ addUtilities }) => {
 	addUtilities({
 		'.link': {
 			'@apply text-brand underline': {},
+			'@apply inline-block duration-150 ease-in-out origin-bottom-left hover:scale-[.97]': {},
 			'@apply focus-visible:!outline-dotted focus-visible:!outline-1 focus-visible:!outline-brand': {},
 		},
 	})
