24cd320ff9e7f00d487312e868edb22d0d9626a9
babakfp <44144724+babakfp@users.noreply.github.com>
2022-11-05 13:54:15 -0700

    some of header

---
+++ src/lib/parts/Header/Index.svelte
@@ -0,0 +1,10 @@
+<header>
+
+	<a class="flex" href="/profile">
+		<div class="w-20 min-w-20 bg-gray-900 rounded-full">
+			<!-- svelte-ignore a11y-img-redundant-alt -->
+			<img class="border-3 border-transparent rounded-full aspect-square" src="https://pbs.twimg.com/profile_images/1557379074322046978/nopS1woe_200x200.png" alt="Your profile image" />
+		</div>
+	</a>
+
+</header>
